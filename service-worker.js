f558d019b22806ec77bd1ae7e60263790f4542bb
Larissa <larissarf1910@gmail.com>
2023-03-12 22:44:54 -0300

    adicionando service-worker

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+var cacheName = 'pwaTeste+-v1.2';
+
+self.addEventListener('install', event => {
+
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll([
+
+        './index.html',
+
+        './assets/css/bootstrap.min.css',
+
+        './assets/js/bootstrap.min.js',
+      ]))
+  );
+});
+
+self.addEventListener('message', function (event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('fetch', function (event) {
+  //Atualizacao internet
+  event.respondWith(async function () {
+     try {
+       return await fetch(event.request);
+     } catch (err) {
+       return caches.match(event.request);
+     }
+   }());
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+
+});
